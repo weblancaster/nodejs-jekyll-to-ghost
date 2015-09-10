f724e8ce8afa88740b75371f4db80201d8550200
MLancaster <mlancaster@mlancaster-m1.local>
2015-09-10 16:16:49 -0500

    re-write in es6/es2015. closes #22 also closes #23 although this is not really FP

--- index.js
+++ index.js
@@ -2,130 +2,101 @@
 
 'use strict';
 
-var path = require('path')
-    , fs = require('fs')
-    , yaml = require('js-yaml')
-    , uuid = require('node-uuid')
-    , clc = require('cli-color')
-    , markdown = require( "markdown" ).markdown
-    , path = require('path');
+let path = require('path');
+let fs = require('fs');
+let yaml = require('js-yaml');
+let uuid = require('node-uuid');
+let clc = require('cli-color');
+let markdown = require( "markdown" ).markdown;
 
 // default color messages
-var logError = clc.red.bold;
-var logWarn = clc.yellow;
-var logNotice = clc.blue;
-var logSuccess = clc.green;
+let logError = clc.red.bold;
+let logWarn = clc.yellow;
+let logNotice = clc.blue;
+let logSuccess = clc.green;
 
-/**
- * Class/Constructor JekyllToGhost responsible to bootstrap the conversion to Ghost format (json)
- * @param {[string]} posts [contain path to Jekyll posts]
- * @class JekyllToGhost
- */
-function JekyllToGhost(pathPosts) {
-    this.folder = pathPosts;
-    this.ghostFileOutput = './ghost-generated.json';
-    this.ghostObj = {
-        data: {
-            posts: []
-        }
-    };
 
-    this.populateGhostData();
-}
+class JekyllToGhost {
 
-/**
- * Call methods to populate needed data format
- * @method populateGhostData
- */
-JekyllToGhost.prototype.populateGhostData = function() {
-    this.populateMeta();
-    this.readPosts();
-}
+    /**
+     * Responsible to startup the class
+     * @param pathPosts [path where jekyll posts are stored]
+     * @method constructor
+     */
+    constructor (pathPosts) {
+        this.folder = pathPosts;
+        this.ghostFileOutput = './ghost-generated.json';
+        this.ghostObj = {
+            data: {
+                posts: []
+            }
+        };
 
-/**
- * Extract post date
- * @param  {[string]} content [post]
- * @return {[string]}         [date]
- * @method extractPostDate
- */
-JekyllToGhost.prototype.extractPostDate = function(content) {
-    return content.substring(0, 10)
-}
-
-/**
- * Extract post name
- * @param  {[string]} content [post]
- * @return {[string]}         [name]
- * @method extractPostName
- */
-JekyllToGhost.prototype.extractPostName = function(content) {
-    return content.substring(11, content.indexOf('.'));
-}
-
-/**
- * Extract post YAML
- * @param  {[string]} content [post]
- * @return {[string]}      [YAML]
- * @method extractYAML
- */
-JekyllToGhost.prototype.extractPostYAML = function(content) {
-    return yaml.safeLoad( content.substring(0, content.indexOf('---', content.indexOf('---') + 1) ));
-}
-
-/**
- * Extract post Markdown
- * @param  {[string]} content [post]
- * @return {[string]}      [Markdown]
- * @method extractMarkdown
- */
-JekyllToGhost.prototype.extractPostMarkdown = function(content) {
-    return content.substring(content.lastIndexOf('---') + 3, content.length);
-}
-
-/**
- * Read each post to fill the json format
- * @method readPosts
- */
-JekyllToGhost.prototype.readPosts = function() {
-    var post, postName, postDate, postPath,
-        postContent, postYAML, postMarkdown
-        , data
-        , self = this
-        , folder = this.folder
-        , re = /(\.md|\.markdown)$/i;
-
-    if ( ! fs.existsSync(folder) ) {
-        console.log( logWarn('Folder > ' + folder + ' < does not exists.') );
-        console.log( logWarn('Make sure to include a folder with Jekyll markdown files inside.') );
-
-        return;
+        this.populateGhostData();
     }
 
-    fs.readdir(folder, function(error, files) {
-        if ( error || files.length < 1 ) {
-            console.log( logWarn('Can not read files at ' + folder) );
-            return;
+    /**
+     * Call methods to populate data format
+     * @method populateGhostData
+     */
+    populateGhostData () {
+        this.populateMeta();
+        this.readPosts();
+    }
+
+    /**
+     * Extract content from markdown post
+     * create post json object
+     * @method readPosts
+     */
+    readPosts () {
+        let post;
+        let postName; 
+        let postDate; 
+        let postPath;
+        let postContent;
+        let postYAML; 
+        let postMarkdown;
+        let data;
+        let folder = this.folder;
+        let re = /(\.md|\.markdown)$/i;
+
+        if ( ! fs.existsSync(folder) ) {
+            console.log( logWarn(`Folder > ${folder} < does not exists.`) );
+            console.log( logWarn('Make sure to enter the right path to jekyll folder containing the markdown files.') );
+
+            return false;
         }
 
-        for ( var i = 0; i < files.length; i++ ) {
-            var postObj = {};
-            post = files[i];
-            postPath = path.join(folder, post);
+        fs.readdir(folder, (error, files) => {
+            if ( error || files.length < 1 ) {
+                console.log( logWarn(`Cant read files at ${folder}`) );
+                return;
+            }
 
-            if ( re.exec(post) ) {
-                postName = self.extractPostName(post);
-                postDate = self.extractPostDate(post);
+            for ( let i = 0; i < files.length; i++ ) {
+                let postObj = {};
+                post = files[i];
+                postPath = path.join(folder, post);
+
+                if ( ! re.exec(post) ) {
+                    console.log( logWarn(`Something went wrong reading post ${post}`) );
+                    continue;
+                }
+
+                postName = this.extractPostName(post);
+                postDate = this.extractPostDate(post);
 
                 data = fs.readFileSync(postPath);
 
                 if ( ! data ) {
-                    console.log( logWarn('Something went wrong at ' + post) );
+                    console.log( logWarn(`Something went wrong reading post ${post}`) );
                     return;
                 }
 
                 postContent = data.toString();
-                postYAML = self.extractPostYAML(postContent);
-                postMarkdown = self.extractPostMarkdown(postContent);
+                postYAML = this.extractPostYAML(postContent);
+                postMarkdown = this.extractPostMarkdown(postContent);
 
                 postObj['id'] = i;
                 postObj['uuid'] = uuid.v4();
@@ -148,55 +119,95 @@ JekyllToGhost.prototype.readPosts = function() {
                 postObj['published_at'] = Date.parse(postDate);
                 postObj['published_by'] = 1;
 
-                self.populatePosts(postObj);
+                this.populatePosts(postObj);
 
-                if ( (self.ghostObj.data.posts.length + 1) === files.length ) {
-                    self.writeToFile();
+                if ( (this.ghostObj.data.posts.length + 1) === files.length ) {
+                    this.writeToFile();
                 }
+
             }
-
-        }
-
-     })
-}
-
-/**
- * Generate ghost json file output
- * @method writeToFile
- */
-JekyllToGhost.prototype.writeToFile = function() {
-    var data = this.ghostToJson()
-
-    fs.writeFileSync(this.ghostFileOutput, data, 'utf8');
-    console.log( logSuccess('Ghost JSON generated successfully!') );
-}
-
-/**
- * Populate meta obj
- * @method populateMeta
- */
-JekyllToGhost.prototype.populateMeta = function() {
-    this.ghostObj['meta'] = {
-        'exported_on': Date.now(),
-        'version': '000'
+        });
     }
-}
 
-/**
- * Populate posts Array with post obj
- * @param  {[obj]} postObj [post obj formatted]
- * @method populatePosts
- */
-JekyllToGhost.prototype.populatePosts = function(postObj) {
-    this.ghostObj['data']['posts'].push(postObj);
-}
+    /**
+     * Extract date from post
+     * @param content [post content]
+     * @returns {string}
+     * @method extractPostDate
+     */
+    extractPostDate (content) {
+        return content.substring(0, 10)
+    }
 
-/**
- * Transform Ghost obj into an Array
- * @return {[JSON]} [Ghost data in json format]
- */
-JekyllToGhost.prototype.ghostToJson = function() {
-    return JSON.stringify(this.ghostObj)
+    /**
+     * Extract name post
+     * @param content [post content]
+     * @returns {string}
+     * @method extractPostName
+     */
+    extractPostName (content) {
+        return content.substring(11, content.indexOf('.'));
+    }
+
+    /**
+     * Extract post YAML header information
+     * @param content [post content]
+     * @returns {yaml}
+     * @method extractPostYAML
+     */
+    extractPostYAML (content) {
+        return yaml.safeLoad( content.substring(0, content.indexOf('---', content.indexOf('---') + 1) ));
+    }
+
+    /**
+     * Extract post markdown content
+     * @param content [post content]
+     * @returns {string}
+     * @method extractPostMarkdown
+     */
+    extractPostMarkdown (content) {
+        return content.substring(content.lastIndexOf('---') + 3, content.length);
+    }
+
+    /**
+     * Populate meta obj
+     * @method populateMeta
+     */
+    populateMeta () {
+        this.ghostObj['meta'] = {
+            'exported_on': Date.now(),
+            'version': '000'
+        }
+    }
+
+    /**
+     * Populate posts Array with post obj
+     * @param  postObj [post obj formatted]
+     * @method populatePosts
+     */
+    populatePosts (postObj) {
+        this.ghostObj['data']['posts'].push(postObj);
+    }
+
+    /**
+     * Parse js obj to json string format
+     * @method ghostToJson
+     */
+    ghostToJson () {
+        return JSON.stringify(this.ghostObj)
+    }
+
+    /**
+     * Write json data to file
+     * @method writeToFile
+     */
+    writeToFile () {
+        let data = this.ghostToJson();
+
+        fs.writeFileSync(this.ghostFileOutput, data, 'utf8');
+        console.log( logSuccess('Ghost JSON generated successfully!') );
+    }
+
 }
 
 
@@ -207,7 +218,7 @@ JekyllToGhost.prototype.ghostToJson = function() {
 console.log( logSuccess('Running...') );
 
 if ( process.argv[2] ) {
-  var app = new JekyllToGhost(process.argv[2]);
+  let app = new JekyllToGhost(process.argv[2]);
 } else {
     console.log( logWarn('You need to specify a path to Jekyll posts.') );
 }
